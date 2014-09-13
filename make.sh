7a628da6977b90f8357b4fe6643ac58a7ffe61ec
Igor Kolar <igor.kolar@gmail.com>
2014-09-13 10:23:05 +0200

    Makeshift build/run script. This project is too small to use maven etc.

---
+++ make.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+
+where=$(dirname $0 | xargs readlink -f)
+
+srcdir="$where/src"
+bindir="$where/bin"
+
+mkdir -p "$srcdir" "$bindir"
+
+javac -cp $(ls -1 lib/*.jar | tr '\n' ':')"." -d "$bindir" "$srcdir"/*.java
---
+++ run.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+
+where=$(dirname $0 | xargs readlink -f)
+
+bindir="$where/bin"
+
+java -cp $(ls -1 "$where"/lib/*.jar | tr '\n' ':')"$bindir:." AcceptAllChanges $@
