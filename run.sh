5afa086f53e7dce3e78ca548f51078b909cf59bd
Igor Kolar <ike@kiberpipa.org>
2014-09-15 08:40:08 +0200

    a more generic run script (yes I know I should probably switch to maven or sth)

--- run.sh
+++ run.sh
@@ -4,4 +4,4 @@ where=$(dirname $0 | xargs readlink -f)
 
 bindir="$where/bin"
 
-java -cp $(ls -1 "$where"/lib/*.jar | tr '\n' ':')"$bindir:." AcceptAllChanges $@
+java -cp $(ls -1 "$where"/lib/*.jar | tr '\n' ':')"$bindir:." $@
